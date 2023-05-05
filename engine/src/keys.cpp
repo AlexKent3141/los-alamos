18cea747edb85392d446ffda972b72a7cc5e32b1
Alex Kent <alex.kent1@hotmail.com>
2023-05-05 20:37:07 +0100

    Attempt to make keys more random.

--- engine/src/keys.cpp
+++ engine/src/keys.cpp
@@ -8,6 +8,11 @@ std::uint64_t keys::piece_square_keys[2][num_piece_types][padded_board_area] = {
 
 keys::keys()
 {
+  struct PRNG
+  {
+    std::uint64_t x, y;
+  };
+
   // Generate the hash keys.
   const auto xorshift64star = [] (std::uint64_t& x)
   {
@@ -17,10 +22,18 @@ keys::keys()
     return x * 0x2545F4914F6CDD1DULL;
   };
 
-  // Seed
-  std::uint64_t x = 0x6ADC22FF67CDB2AF;
+  // The top 32 bits are more random, so combine the top words from two instances.
+  const auto prng_step = [xorshift64star] (PRNG& prng)
+  {
+    std::uint64_t word = (xorshift64star(prng.x) >> 32) << 32;
+    word |= (xorshift64star(prng.y) >> 32);
+    return word;
+  };
 
-  white_key = xorshift64star(x);
+  // Seed
+  PRNG prng = { 0x6ADC22FF67CDB2AF, 0xDEADBEEF12345678 };
+
+  white_key = prng_step(prng);
 
   for (int col = 0; col < 2; col++)
   {
@@ -28,7 +41,7 @@ keys::keys()
     {
       for (int loc = 0; loc < padded_board_area; loc++)
       {
-        piece_square_keys[col][pt][loc] = xorshift64star(x);
+        piece_square_keys[col][pt][loc] = prng_step(prng);
       }
     }
   }
