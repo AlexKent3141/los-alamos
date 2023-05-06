f9473716049bcaa27e3b45bcf5049654817baaa2
Alex Kent <alex.kent1@hotmail.com>
2023-05-06 18:36:56 +0100

    Added tool which performs search on the initial position.

---
+++ tools/search_test.cpp
@@ -0,0 +1,27 @@
+#include "search/search.h"
+
+#include <cstdio>
+#include <cstdlib>
+
+int main()
+{
+  la::Board board;
+
+  const auto callback = [&board] (const la::SearchData& data)
+  {
+    std::printf(
+      "%6d %6s %7d %13ld\n",
+      data.depth,
+      board.move_to_string(data.best_move).c_str(),
+      data.score,
+      data.nodes_searched);
+
+    std::fflush(stdout);
+  };
+
+  constexpr std::chrono::milliseconds search_time{60000};
+
+  la::search(board, search_time, callback);
+
+  return EXIT_SUCCESS;
+}
